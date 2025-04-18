044c734ff0c6412d82ce329ceacf45f0dc391111
Yash <shindeyash0204@gmail.com>
2025-04-18 14:43:51 +0530

    Updated PWA config for production

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,13 @@ import withPWA from 'next-pwa'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // 👇 Needed for production-ready serverless hosting (Vercel)
+  output: 'standalone',
 }
 
 export default withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // disable PWA during dev to avoid issues
 })(nextConfig)
